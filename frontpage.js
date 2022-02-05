c4a14d0a07ae11ba57887bd7b9d7fdae4131309e
kyon-katsube <kyokong0329@gmail.com>
2022-02-06 08:19:31 +0900

    サイドバーbackground質問0206

--- frontpage.js
+++ frontpage.js
@@ -16,43 +16,42 @@ $(function(){
         $(".p-sidebar").removeClass('slideactive');//サイドバーが右に隠れる
       });
 
+//ナビメニューが出たら背景を固定する（SP/TAB版）
+    //変数の設定
+    var $body = $('body');
 
-     //ナビメニューが出たら背景を固定する（SP/TAB版）
-//変数の設定
-var $body = $('body');
+    //スクロール量を保存
+    var scrollTop;
 
-//スクロール量を保存
-var scrollTop;
+    //スクロールを固定
+    function bodyFixedOn() {
+    scrollTop = $(window).scrollTop();
 
-//スクロールを固定
-function bodyFixedOn() {
-  scrollTop = $(window).scrollTop();
-  
-  $body.css({
-    position: 'fixed',
-    top: -scrollTop
-  });
-}
+    $body.css({
+      position: 'fixed',
+      top: -scrollTop
+    });
+    }
 
-//スクロールの固定を解除
-function bodyFixedOff() {
-  $body.css({
-    position: '',
-    top: ''
-  });
-   
-  $(window).scrollTop(scrollTop);
-}
+    //スクロールの固定を解除
+    function bodyFixedOff() {
+    $body.css({
+      position: '',
+      top: ''
+    });
+      
+    $(window).scrollTop(scrollTop);
+    }
 
-//モーダルのトリガーをクリックしたとき
-$('.c-btn--menu').on('click', function() {
-  bodyFixedOn();
-});
+    //モーダルのトリガーをクリックしたとき
+    $('.c-btn--menu').on('click', function() {
+    bodyFixedOn();
+    });
 
-//モーダルの閉じるボタンをクリックしたとき
-$('.c-btn--close').on('click', function() {
-  bodyFixedOff();
-});
+    //モーダルの閉じるボタンをクリックしたとき
+    $('.c-btn--close').on('click', function() {
+    bodyFixedOff();
+    });
 
 
 
