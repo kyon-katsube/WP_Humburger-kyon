38ceffb2d899a67ee51013a55681744e9a9faaf3
kyon-katsube <kyokong0329@gmail.com>
2022-02-16 15:19:34 +0900

    flocss化footerまで0216

--- frontpage.js
+++ frontpage.js
@@ -5,7 +5,7 @@ $(function(){
     $(".c-button--menu--sp").click(function () {//Menuボタンがクリックされたら
         $(".c-button--cross").toggleClass('active');//×ボタンを activeクラスを付与して表示      
         $(".c-wrapper").toggleClass('backactive');//空要素のクラスにpanelactiveクラスを付与
-        $(".p-sidebar").toggleClass('slideactive'); //サイドバーが出現
+        $(".l-sidebar").toggleClass('slideactive'); //サイドバーが出現
       });
       
       console.log('test'); 
@@ -13,7 +13,7 @@ $(function(){
     $(".c-button--cross").click(function () {//×ボタンがクリックされたら
         $(this).removeClass('active');//ボタンの activeクラスを除去し
         $(".c-wrapper").removeClass('backactive');//ナビゲーションのpanelactiveクラスも除去
-        $(".p-sidebar").removeClass('slideactive');//サイドバーが右に隠れる
+        $(".l-sidebar").removeClass('slideactive');//サイドバーが右に隠れる
       });
 
 //ナビメニューが出たら背景を固定する（SP/TAB版）
